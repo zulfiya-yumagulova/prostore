945c6c4e745e8278a41526cf9b5d9306fb581170
Zulfiya Yumagulova <yumagulovazu@gmail.com>
2025-03-04 21:46:18 +0000

    create ProductList component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,14 @@
+import dummyData from '@/db/dummy-data';
+import ProductList from '@/components/shared/product/product-list';
+
 export default function Homepage() {
-  return <>Click</>;
+  return (
+    <>
+      <ProductList
+        data={dummyData.products}
+        title="Featured Products"
+        limit={4}
+      />
+    </>
+  );
 }
---
+++ components/shared/product/product-list.tsx
@@ -0,0 +1,29 @@
+const ProductList = ({
+  data,
+  title,
+  limit,
+}: {
+  data: any;
+  title?: string;
+  limit?: number;
+}) => {
+  const limitedProduct = limit ? data.slice(0, limit) : data;
+  return (
+    <div className="my-10">
+      <h2 className="h2-bold mb-4">{title}</h2>
+      {data.length > 0 ? (
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-col-3 lg:grid-cols-4 gap-4">
+          {limitedProduct.map((product: any) => (
+            <div>{product.name}</div>
+          ))}
+        </div>
+      ) : (
+        <div>
+          <p>No products found</p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default ProductList;
