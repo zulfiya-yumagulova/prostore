c9b044963c31b4bc74adb1354f491210285b135a
Zulfiya Yumagulova <yumagulovazu@gmail.com>
2025-03-04 22:06:49 +0000

    create product price component

--- components/shared/product/product-card.tsx
+++ components/shared/product/product-card.tsx
@@ -1,6 +1,7 @@
 import { Card, CardContent, CardHeader } from '@/components/ui/card';
 import Image from 'next/image';
 import Link from 'next/link';
+import ProductPrice from './product-price';
 
 const ProductCard = ({ product }: { product: any }) => {
   return (
@@ -24,7 +25,7 @@ const ProductCard = ({ product }: { product: any }) => {
         <div className="flex-betwee gap-4">
           <p>{product.rating} Stars</p>
           {product.stock > 0 ? (
-            <p className="font-bold">£ {product.price}</p>
+            <ProductPrice value={product.price} />
           ) : (
             <p className="text-destrictive">Out Of Stock</p>
           )}
---
+++ components/shared/product/product-price.tsx
@@ -0,0 +1,24 @@
+import { cn } from '@/lib/utils';
+
+const ProductPrice = ({
+  value,
+  className,
+}: {
+  value: number;
+  className?: string;
+}) => {
+  // Ensure two decimal places
+  const stringValue = value.toFixed(2);
+  // get the int and float
+  const [intValue, floatValue] = stringValue.split('.');
+
+  return (
+    <p className={cn('text-2xl', className)}>
+      <span className="text-xs align-super">£</span>
+      {intValue}
+      <span className="text-xs align-super">.{floatValue}</span>
+    </p>
+  );
+};
+
+export default ProductPrice;
