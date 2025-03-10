37ffcc7913bae6bccd03ebf92a6e1f070eb3c60a
Zulfiya Yumagulova <yumagulovazu@gmail.com>
2025-03-10 19:15:52 +0000

    add product images component

--- app/(root)/product/[slug]/page.tsx
+++ app/(root)/product/[slug]/page.tsx
@@ -4,6 +4,7 @@ import { Badge } from '@/components/ui/badge';
 import { Button } from '@/components/ui/button';
 import { Card, CardContent } from '@/components/ui/card';
 import ProductPrice from '@/components/shared/product/product-price';
+import ProductImages from '@/components/shared/product/product-images';
 
 const ProductDetailsPage = async (props: {
   params: Promise<{ slug: string }>;
@@ -20,7 +21,9 @@ const ProductDetailsPage = async (props: {
       <section>
         <div className="grid grid-cols-1 md:grid-cols-5">
           {/* Images column */}
-          <div className="col-span-2">{/* Images compoenet */}</div>
+          <div className="col-span-2">
+            <ProductImages images={product.images} />
+          </div>
           {/* Details column */}
           <div className="col-span-2 p-5">
             <div className="flex flex-col gap-6">
---
+++ components/shared/product/product-images.tsx
@@ -0,0 +1,38 @@
+'use client';
+import { useState } from 'react';
+import Image from 'next/image';
+import { cn } from '@/lib/utils';
+
+const ProductImages = ({ images }: { images: string[] }) => {
+  const [current, setCurrent] = useState(0);
+
+  return (
+    <div className="space-y-4">
+      <Image
+        src={images[current]}
+        alt="product image"
+        width={1000}
+        height={1000}
+        className="min-h-[300px]
+        object-cover
+        object-center"
+      />
+      <div className="flex">
+        {images.map((image, index) => (
+          <div
+            key={index}
+            onClick={() => setCurrent(index)}
+            className={cn(
+              'border mr-2 cursor-pointer hover:border-orange-600',
+              current === index && 'border-orange-500'
+            )}
+          >
+            <Image src={image} alt="image" width={100} height={100} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ProductImages;
