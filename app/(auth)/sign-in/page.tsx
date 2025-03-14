1ddd58ad6978f27e8db2c924096049d68b1e05b3
Zulfiya Yumagulova <yumagulovazu@gmail.com>
2025-03-14 16:57:23 +0000

    add auth layout and sign in page

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,43 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+import { APP_NAME } from '@/lib/constans';
+import { Metadata } from 'next';
+import Image from 'next/image';
+import Link from 'next/link';
+
+export const metadata: Metadata = {
+  title: 'Sign In',
+};
+
+const SignInPage = () => {
+  return (
+    <div className="w-full max-w-md mx-auto">
+      <Card>
+        <CardHeader className="space-y-4">
+          <Link href="/" className="flex-center">
+            <Image
+              src="/images/logo.svg"
+              width={100}
+              height={100}
+              alt={`${APP_NAME}`}
+              priority={true}
+            ></Image>
+          </Link>
+
+          <CardTitle className="text-center">Sign In</CardTitle>
+          <CardDescription className="text-center">
+            Sign in into ypur account
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="space-y-4">{/* Form here */}</CardContent>
+      </Card>
+    </div>
+  );
+};
+
+export default SignInPage;
