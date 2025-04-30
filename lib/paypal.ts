93ad2eafb82d8b8c55264acb3112ab6224199241
Zulfiya Yumagulova <yumagulovazu@gmail.com>
2025-04-30 17:37:56 +0100

    create order & order capture requests

--- lib/paypal.ts
+++ lib/paypal.ts
@@ -1,6 +1,46 @@
 const base = process.env.PAYPAL_API_URL || 'https://api-m.sandbox.paypal.com';
 
-export const paypal = {};
+export const paypal = {
+  createOrder: async function createOrder(price: number) {
+    const accessToken = await generateAccessToken();
+    const url = `${base}/v2/checkout/orders`;
+
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${accessToken}`,
+      },
+
+      body: JSON.stringify({
+        intent: 'CAPTURE',
+        purchase_units: [
+          {
+            amount: {
+              currency_code: 'GBP',
+              value: price,
+            },
+          },
+        ],
+      }),
+    });
+
+    return await handleResponse(response);
+  },
+
+  capturePayment: async function capturePayment(orderId: string) {
+    const accessToken = await generateAccessToken();
+    const url = `${base}/v2/checkout/orders/${orderId}/capture`;
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+    return handleResponse(response);
+  },
+};
 
 // Genrate a new paypal access token
 async function generateAccessToken() {
@@ -18,9 +58,14 @@ async function generateAccessToken() {
     },
   });
 
+  const jsonData = await handleResponse(response);
+  return jsonData.access_token;
+}
+
+async function handleResponse(response: Response) {
   if (response.ok) {
-    const jsonData = await response.json();
-    return jsonData.access_token;
+    const jsonData = response.json();
+    return jsonData;
   } else {
     const errorMessage = await response.text();
     throw new Error(errorMessage);
