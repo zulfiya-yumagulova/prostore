c5c559c6379bbaf6bebe0904f20830ae85dcf76f
Zulfiya Yumagulova <yumagulovazu@gmail.com>
2025-05-16 15:11:36 +0100

    add update user's name action

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -145,3 +145,36 @@ export async function updateUserPaymentMethod(
     return { success: false, message: formatError(error) };
   }
 }
+
+// Update the User's profile
+export async function updateProfile(user: {
+  id: string | undefined;
+  name: string;
+  email: string;
+}) {
+  try {
+    const session = await auth();
+
+    // Update user name
+    const currentUser = await prisma.user.findFirst({
+      where: {
+        id: session?.user?.id,
+      },
+    });
+
+    if (!currentUser) throw new Error('User not found');
+
+    await prisma.user.update({
+      where: {
+        id: currentUser.id,
+      },
+      data: {
+        name: user.name,
+      },
+    });
+
+    return { success: true, message: 'User updated successfully!' };
+  } catch (error) {
+    return { success: false, message: formatError(error) };
+  }
+}
--- lib/validators.ts
+++ lib/validators.ts
@@ -120,3 +120,9 @@ export const paymentResultSchema = z.object({
   email_address: z.string(),
   pricePaid: z.string(),
 });
+
+// Schema for updating User's profile
+export const updateProfileSchema = z.object({
+  name: z.string().min(3, 'Name must be at least 3 characters'),
+  email: z.string().min(3, 'Email must be at least 3 characters'),
+});
