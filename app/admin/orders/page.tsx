f0851c033ee76bf4010736f9338ad958f5720d7a
Zulfiya Yumagulova <yumagulovazu@gmail.com>
2025-05-20 14:26:37 +0100

    add admin orders page and actions

---
+++ app/admin/orders/page.tsx
@@ -0,0 +1,86 @@
+import { auth } from '@/auth';
+import { getAllOrders } from '@/lib/actions/order.actions';
+import { Metadata } from 'next';
+import Link from 'next/link';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table';
+import Pagination from '@/components/shared/pagination';
+import { formatCurrency, formatDateTime, formatId } from '@/lib/utils';
+import { Button } from '@/components/ui/button';
+
+export const metadata: Metadata = {
+  title: 'Admin Orders',
+};
+
+const AdminOrderPage = async (props: {
+  searchParams: Promise<{ page: string }>;
+}) => {
+  const { page = '1' } = await props.searchParams;
+
+  const session = await auth();
+
+  if (session?.user.role !== 'admin') throw new Error('User is not authorized');
+
+  const orders = await getAllOrders({ page: Number(page) });
+
+  return (
+    <div className="space-y-2">
+      <h2 className="h2-bold">Orders</h2>
+      <div className="overflow-x-auto">
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>ID</TableHead>
+              <TableHead>DATE</TableHead>
+              <TableHead>TOTAL</TableHead>
+              <TableHead>PAID</TableHead>
+              <TableHead>DELIVERED</TableHead>
+              <TableHead>ACTIONS</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {orders.data.map((order) => (
+              <TableRow key={order.id}>
+                <TableCell>{formatId(order.id)}</TableCell>
+                <TableCell>
+                  {formatDateTime(order.createdAt).dateTime}
+                </TableCell>
+                <TableCell>{formatCurrency(order.totalPrice)}</TableCell>
+                <TableCell>
+                  {order.isPaid && order.paidAt
+                    ? formatDateTime(order.paidAt).dateTime
+                    : 'Not Paid'}
+                </TableCell>
+                <TableCell>
+                  {order.isDelivered && order.delivereddAt
+                    ? formatDateTime(order.delivereddAt).dateTime
+                    : 'Not Delivered'}
+                </TableCell>
+                <TableCell>
+                  <Button asChild variant="outline" size="sm">
+                    <Link href={`/order/${order.id}`}>Details</Link>
+                  </Button>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+
+        {orders.totalPages > 1 && (
+          <Pagination
+            page={Number(page) || 1}
+            totalPages={orders?.totalPages}
+          />
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default AdminOrderPage;
--- lib/actions/order.actions.ts
+++ lib/actions/order.actions.ts
@@ -338,3 +338,26 @@ export async function getOrderSummary() {
     salesData,
   };
 }
+
+// Get all orders
+export async function getAllOrders({
+  limit = PAGE_SIZE,
+  page,
+}: {
+  limit?: number;
+  page: number;
+}) {
+  const data = await prisma.order.findMany({
+    orderBy: { createdAt: 'desc' },
+    take: limit,
+    skip: (page - 1) * page,
+    include: { user: { select: { name: true } } },
+  });
+
+  const dataCount = await prisma.order.count();
+
+  return {
+    data,
+    totalPages: Math.ceil(dataCount / limit),
+  };
+}
